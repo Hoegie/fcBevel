c403dbacea8ef6fa35b5f68b55eee990ea1aa87a
Hoegie <mezenhof8@gmail.com>
2018-07-10 15:32:08 +0200

    1st commit

---
+++ footcalhook.sh
@@ -0,0 +1,3 @@
+date >> /app/nodeprojects/github/logs/footcalhook_5003.log 2>&1
+cd /app/nodeprojects/github/fcBevel
+git pull https://github.com/Hoegie/fcBevel.git >> /app/nodeprojects/github/logs/footcalhook_5003.log 2>&1
---
+++ footcalini.js
@@ -0,0 +1,8 @@
+module.exports.clubID = '3';
+module.exports.clubName = 'FC BEVEL';
+module.exports.apisec = false;
+module.exports.dbname = 'FootCal_fcBevel';
+module.exports.apachedir = 'fcBevel';
+module.exports.apiport = 4003;
+module.exports.serveraddress = "185.224.89.139";
+
