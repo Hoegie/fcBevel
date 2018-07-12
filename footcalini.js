1369a86cc72c49e728c56e80d9e6b985cd40b8c4
Hoegie <mezenhof8@gmail.com>
2018-07-12 11:37:41 +0200

    nth commit

--- footcalini.js
+++ footcalini.js
@@ -1,5 +1,6 @@
 module.exports.clubID = '3';
 module.exports.clubName = 'FC BEVEL';
+module.exports.clubBaseNr = '1640';
 module.exports.apisec = false;
 module.exports.dbname = 'FootCal_fcBevel';
 module.exports.apachedir = 'fcBevel';
